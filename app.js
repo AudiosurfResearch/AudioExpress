a60108a204e0e4edb9f1d48f1043c33ef4414f21
RubberDuckShobe <dog@dog.dog>
2022-02-05 18:16:50 +0100

    Remove pug and related routes

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var as1apiRouter = require('./routes/as1_api');
 const { json } = require('body-parser');
 var bodyParser = require('body-parser');
@@ -12,12 +10,7 @@ let dotenv = require('dotenv').config();
 
 var app = express();
 
-//app.disable('etag');
 app.disable('x-powered-by');
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -25,10 +18,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/as', as1apiRouter);
 //Why is this a thing
 app.use('//as', as1apiRouter);
@@ -36,17 +26,6 @@ app.use('//as', as1apiRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = app;
